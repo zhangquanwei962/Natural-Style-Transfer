bec192f8dee3bb47d92b66c369b74d8385f39711
zhangquanwei <2875569757@qq.com>
2022-10-09 14:35:47 +0800

    nst

---
+++ li mu/main.py
@@ -0,0 +1,145 @@
+import torch
+import torchvision
+from torch import nn
+import copy
+from PIL import Image
+import matplotlib.pyplot as plt
+
+def try_all_gpu():
+    devices = [torch.device(f'cuda:{i}') for i in range(torch.cuda.device_count())]
+    return devices if devices else [torch.device('cpu')]
+
+
+
+def imshow(tensor, title=None):
+    image = tensor.cpu().clone()  # we clone the tensor to not do changes on it
+    #image = image.squeeze(0)      # remove the fake batch dimension
+    image = postprocess(image)
+    plt.imshow(image)
+    if title is not None:
+        plt.title(title)
+    plt.pause(0.001) # pause a bit so that plots are updated
+
+content_img = Image.open('../Gatys/data/rainier.jpg')
+style_img = Image.open('../Gatys/data/autumn-oak.jpg')
+
+rgb_mean = torch.tensor([0.485, 0.456, 0.406])
+rgb_std = torch.tensor([0.229, 0.224, 0.225])
+
+def preprocess(img, image_shape):
+    transforms = torchvision.transforms.Compose([
+        torchvision.transforms.Resize(image_shape),
+        torchvision.transforms.ToTensor(),
+        torchvision.transforms.Normalize(mean=rgb_mean, std=rgb_std)])
+    return transforms(img).unsqueeze(0)
+
+def postprocess(img):
+    img = img[0].to(rgb_std.device)
+    img = torch.clamp(img.permute(1, 2, 0) * rgb_std + rgb_mean, 0, 1)
+    #print(torchvision.transforms.ToPILImage()(img.permute(2, 0, 1)).shape)
+    return torchvision.transforms.ToPILImage()(img.permute(2, 0, 1))
+
+pretrained_net = torchvision.models.vgg19(pretrained=True)
+
+style_layers, content_layers = [0, 5, 10, 19, 28], [25]
+
+net = nn.Sequential(*[pretrained_net.features[i] for i in
+                      range(max(content_layers + style_layers) + 1)])
+
+def extract_features(X, content_layers, style_layers):
+    contents = []
+    styles = []
+    for i in range(len(net)):
+        X = net[i](X)
+        if i in style_layers:
+            styles.append(X)
+        if i in content_layers:
+            contents.append(X)
+    return contents, styles
+
+def get_contents(image_shape, device):
+    content_X = preprocess(content_img, image_shape).to(device)
+    contents_Y, _ = extract_features(content_X, content_layers, style_layers)
+    return content_X, contents_Y
+
+def get_styles(image_shape, device):
+    style_X = preprocess(style_img, image_shape).to(device)
+    _, styles_Y = extract_features(style_X, content_layers, style_layers)
+    return style_X, styles_Y
+
+def content_loss(Y_hat, Y):
+    # 我们从动态计算梯度的树中分离目标：
+    # 这是一个规定的值，而不是一个变量。
+    return torch.square(Y_hat - Y.detach()).mean()
+
+def gram(X):
+    num_channels, n = X.shape[1], X.numel() // X.shape[1]
+    X = X.reshape((num_channels, n))
+    return torch.matmul(X, X.T) / (num_channels * n)
+
+def style_loss(Y_hat, gram_Y):
+    return torch.square(gram(Y_hat) - gram_Y.detach()).mean()
+
+
+def tv_loss(Y_hat):
+    return 0.5 * (torch.abs(Y_hat[:, :, 1:, :] - Y_hat[:, :, :-1, :]).mean() +
+                  torch.abs(Y_hat[:, :, :, 1:] - Y_hat[:, :, :, :-1]).mean())
+
+content_weight, style_weight, tv_weight = 1, 1e3, 10
+
+def compute_loss(X, contents_Y_hat, styles_Y_hat, contents_Y, styles_Y_gram):
+    # 分别计算内容损失、风格损失和全变分损失
+    contents_l = [content_loss(Y_hat, Y) * content_weight for Y_hat, Y in zip(
+        contents_Y_hat, contents_Y)]
+    styles_l = [style_loss(Y_hat, Y) * style_weight for Y_hat, Y in zip(
+        styles_Y_hat, styles_Y_gram)]
+    tv_l = tv_loss(X) * tv_weight
+    # 对所有损失求和
+    l = sum(10 * styles_l + contents_l + [tv_l])
+    return contents_l, styles_l, tv_l, l
+
+class SynthesizedImage(nn.Module):
+    def __init__(self, img_shape, **kwargs):
+        super(SynthesizedImage, self).__init__(**kwargs)
+        self.weight = nn.Parameter(torch.rand(*img_shape))
+
+    def forward(self):
+        return self.weight
+
+def get_inits(X, device, lr, styles_Y):
+    gen_img = SynthesizedImage(X.shape).to(device)
+    gen_img.weight.data.copy_(X.data)
+    trainer = torch.optim.Adam(gen_img.parameters(), lr=lr)
+    styles_Y_gram = [gram(Y) for Y in styles_Y]
+    return gen_img(), styles_Y_gram, trainer
+
+def train(X, contents_Y, styles_Y, device, lr, num_epochs, lr_decay_epoch):
+    X, styles_Y_gram, trainer = get_inits(X, device, lr, styles_Y)
+    scheduler = torch.optim.lr_scheduler.StepLR(trainer, lr_decay_epoch, 0.8)
+    for epoch in range(num_epochs):
+        trainer.zero_grad()
+        contents_Y_hat, styles_Y_hat = extract_features(
+            X, content_layers, style_layers)
+        contents_l, styles_l, tv_l, l = compute_loss(
+            X, contents_Y_hat, styles_Y_hat, contents_Y, styles_Y_gram)
+        l.backward()
+        trainer.step()
+        scheduler.step()
+        if (epoch + 1) % 5 == 0:
+            # 由于风格损失列表有5项，因此算个总损失输出
+            print('迭代次数：{}    内容损失：{:.9f}    风格损失：{:.9f}    总变差损失：{:.9f}' .format(epoch+1, sum(contents_l).item(), sum(styles_l).item(), tv_l.item()))
+    return X
+device, image_shape = try_all_gpu()[0], (300, 450)
+net = net.to(device)
+content_X, contents_Y = get_contents(image_shape, device)
+_, styles_Y = get_styles(image_shape, device)
+output = train(content_X, contents_Y, styles_Y, device, 0.3, 500, 50)
+
+
+plt.figure()
+imshow(output, title='Output Image')
+plt.savefig("test1.pdf") 
+#X = torch.randn(size=(1, 1, 224, 224))
+
+#feature = torch.nn.Sequential(*list(pretrained_net.children())[:])
+#print(feature)
